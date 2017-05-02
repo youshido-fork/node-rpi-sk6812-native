1d6538d315fc58266df5624d5ce231cf6ff238a0
DaVarga <varga.daniel@gmx.de>
2017-05-02 16:34:46 +0200

    update examples

--- examples/iterate.js
+++ examples/iterate.js
@@ -10,8 +10,8 @@ ws281x.init(NUM_LEDS, {
 
 // ---- trap the SIGINT and reset before exit
 process.on('SIGINT', function () {
-  ws281x.reset();
-  process.nextTick(function () { process.exit(0); });
+    ws281x.reset();
+    process.nextTick(function () { process.exit(0); });
 });
 
 
@@ -19,13 +19,13 @@ process.on('SIGINT', function () {
 var offset = 0;
 setInterval(function () {
   var i=NUM_LEDS;
-  while(i--) {
-      pixelData[i] = 0;
-  }
-  pixelData[offset] = 0xffffffff;
+    while(i--) {
+        pixelData[i] = 0;
+    }
+    pixelData[offset] = 0xffffffff;
 
-  offset = (offset + 1) % NUM_LEDS;
-  ws281x.render(pixelData);
+    offset = (offset + 1) % NUM_LEDS;
+    ws281x.render(pixelData);
 }, 100);
 
 console.log('Press <ctrl>+C to exit.');
